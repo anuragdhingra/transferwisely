d679848ebba4009771c252ff38b267d0677fc81a
anurag.dhingra <anurag.dhingra@monstar-lab.com>
2020-04-20 01:33:02 +0530

    Improve error handling for invalid env vars

--- main.go
+++ main.go
@@ -4,13 +4,16 @@ import (
     "fmt"
     "github.com/go-co-op/gocron"
     "net/http"
-    "os"
     "strconv"
     "time"
 )
 
 func main() {
-    interval, _ := strconv.ParseUint(os.Getenv("INTERVAL"), 10, 64)
+    interval, err := strconv.ParseUint(intervalVar, 10, 64)
+    if err != nil {
+        fmt.Printf("Invalid value for INTERVAL: %v", err)
+        return
+    }
 
     s1 := gocron.NewScheduler(time.UTC)
     s1.Every(interval).Minutes().Do(checkAndProcess)
