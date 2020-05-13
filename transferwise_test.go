1696fd67c33b0be598a64fa3609ee4dde83380c2
anurag.dhingra <anurag.dhingra@monstar-lab.com>
2020-05-13 14:03:51 +0530

    add tests for generateQuote

--- transferwise_test.go
+++ transferwise_test.go
@@ -59,3 +59,48 @@ func TestGetDetailByQuoteId(t *testing.T)  {
 
     })
 }
+
+func TestGenerateQuote(t *testing.T)  {
+    t.Run("success", func(t *testing.T) {
+        // build response JSON
+        q := QuoteDetail{}
+        err := faker.FakeData(&q)
+        if err != nil {
+            log.Println(err)
+        }
+        j, _ := json.Marshal(q)
+        // create a new reader with that JSON
+        r := ioutil.NopCloser(bytes.NewReader(j))
+        mocks.GetDoFunc = func(*http.Request) (*http.Response, error) {
+            return &http.Response{
+                StatusCode: http.StatusOK,
+                Body:       r,
+            }, nil
+        }
+
+        qId, err := generateQuote("anything", "anything", 1, 1)
+        assert.NotEmpty(t, qId)
+        assert.Equal(t, q.Id, qId)
+        assert.NoError(t, err)
+    })
+
+    t.Run("external api error", func(t *testing.T) {
+        // build response JSON
+        q := QuoteDetail{}
+        j, _ := json.Marshal(q)
+        // create a new reader with that JSON
+        r := ioutil.NopCloser(bytes.NewReader(j))
+        mocks.GetDoFunc = func(*http.Request) (*http.Response, error) {
+            return &http.Response{
+                StatusCode: http.StatusInternalServerError,
+                Body:       r,
+            }, nil
+        }
+
+        qId, err := generateQuote("anything", "anything", 1, 1)
+        assert.Empty(t, qId)
+        assert.Error(t, err)
+    })
+}
+
+
