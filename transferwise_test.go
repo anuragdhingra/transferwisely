59fa77c04e14b864093c7965ec0fdbf5e254583d
anurag.dhingra <anurag.dhingra@monstar-lab.com>
2020-05-15 01:27:30 +0530

    add cancel transfer tests

--- transferwise_test.go
+++ transferwise_test.go
@@ -3,6 +3,7 @@ package main
 import (
     "bytes"
     "encoding/json"
+    "errors"
     "github.com/bxcodec/faker/v3"
     "github.com/stretchr/testify/assert"
     "io/ioutil"
@@ -99,6 +100,44 @@ func TestGenerateQuote(t *testing.T)  {
 
         qId, err := generateQuote("anything", "anything", 1, 1)
         assert.Empty(t, qId)
+        assert.Error(t, err)
+    })
+}
+
+func TestCancelTransfer(t *testing.T)  {
+    t.Run("success", func(t *testing.T) {
+        // build response JSON
+        tId := uint64(1)
+        j, _ := json.Marshal(tId)
+        // create a new reader with that JSON
+        r := ioutil.NopCloser(bytes.NewReader(j))
+        mocks.GetDoFunc = func(*http.Request) (*http.Response, error) {
+            return &http.Response{
+                StatusCode: http.StatusOK,
+                Body: r,
+            }, nil
+        }
+
+        result, err := cancelTransfer(tId)
+        assert.Equal(t, result, true)
+        assert.NoError(t, err)
+    })
+
+    t.Run("external api error", func(t *testing.T) {
+        // build response JSON
+        err := errors.New("error calling external API")
+        j, _ := json.Marshal(err)
+        // create a new reader with that JSON
+        r := ioutil.NopCloser(bytes.NewReader(j))
+        mocks.GetDoFunc = func(*http.Request) (*http.Response, error) {
+            return &http.Response{
+                StatusCode: http.StatusInternalServerError,
+                Body: r,
+            }, nil
+        }
+
+        result, err := cancelTransfer(1)
+        assert.Equal(t, result, false)
         assert.Error(t, err)
     })
 }
